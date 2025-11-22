7b2d7777de51366db742d0c0675170d7fcf43db2
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-11-21 18:56:35 -0600

    [msdos] sync up.

--- src/msdos/graphics.c
+++ src/msdos/graphics.c
@@ -11,7 +11,6 @@
 #include "../platform-specific/graphics.h"
 #include "../platform-specific/sound.h"
 #include "../misc.h"
-#include "charset.h"
 
 #define VIDEO_RAM_ADDR ((unsigned char far *)0xB8000000UL)
 #define VIDEO_LINE_BYTES 80
@@ -60,8 +59,9 @@ uint16_t legendShipOffset[] = {2, 1, 0, 256U * 5, 256U * 6 + 1};
  * @param tile ptr to 2bpp tile data * 8
  * @param x Column 0-39
  * @param y Row 0-24
+ * @param c Color
  */
-void plot_tile(const unsigned char *tile, unsigned char x, unsigned char y)
+void plot_tile(const unsigned char *tile, unsigned char x, unsigned char y, unsigned char c)
 {
     unsigned char i=0;
 
@@ -137,15 +137,6 @@ void initGraphics()
     r.h.bl = 2;             // Color 2
     r.h.bh = 4;             // To dark red
     int86(0x10,&r,&r);
-
-    // Create a second set of white on black text for status bar
-    // It could also be achieved via a mask in exchange for more memory
-    c=&ascii[0][0];
-    for(i=0;i<sizeof(ascii)/2;i++) {
-        // Set  ascii characters to white on black
-        *(c+1536)= (*c^0x55)*3;
-        c++;
-    }
 }
 
 bool saveScreenBuffer()
@@ -201,7 +192,7 @@ void drawText(uint8_t x, uint8_t y, const char *s)
             y++;
         }
 
-        plot_tile(&ascii[c+mask],x++,y);
+        plot_tile(&ascii[c+],x++,y);
     }
 }
 
